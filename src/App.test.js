b7e2883e5694ee7ff653d8b77519d5a4c90cfa25
Valona <valona.nikaj06@gmail.com>
2025-11-27 10:59:01 +0100

    Fix tests for CI/CD

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/App Ref\. Card 02/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('renders app message', () => {
+  render(<App />);
+  const messageElement = screen.getByText(/react app \.\.\. up and running/i);
+  expect(messageElement).toBeInTheDocument();
 });
